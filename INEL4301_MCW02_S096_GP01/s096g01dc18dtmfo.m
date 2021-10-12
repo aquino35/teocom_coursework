8d8686cc4d8791af3703e01113e5d7941949e356
aquino35 <osvaldo.aquino@upr.edu>
2021-10-11 23:36:17 -0400

    Added MCW02

---
+++ INEL4301_MCW02_S096_GP01/s096g01dc18dtmfo.m
@@ -0,0 +1,46 @@
+%s096g01dc12dtmf 
+%clear all 
+%close all 
+Fs=8192; 
+Ts=1/Fs; 
+%V=10;           %Time Transmission window (TW) 
+t=0:Ts:(81920*Ts)-Ts;    %Time axis for TW 
+tt=0:Ts:(1024*Ts)-Ts;    %Pulse time tone “ON” width 
+tz=0:Ts:(7168*Ts)-Ts;    %Pulse time zero “OFF” width 
+s1=cos(2*pi*697*tt)+cos(2*pi*1209*tt); 
+s3=cos(2*pi*697*tt)+cos(2*pi*1477*tt); 
+s8=cos(2*pi*852*tt)+cos(2*pi*1336*tt); 
+s0=cos(2*pi*941*tt)+cos(2*pi*1336*tt); 
+sz=0*tz;          %Pulse signal segment “OFF” 
+s=[s3 sz s8 sz s1 sz s1 sz s8 sz s8 sz s3 sz s0 sz s3 sz s0 sz]; 
+figure
+plot(t,s)
+axis([0 max(t) -max(abs(s)) max(abs(s))]) 
+xlabel('Time in Seconds') 
+ylabel('Amplitude') 
+title('DTFM Message or Word of Order or Length L=10 and Duty Cycle=1/2 to 1/8') 
+sound(s,Fs)
+
+
+%s096g01dc38dtmf 
+%clear all 
+%close all 
+Fs=8192; 
+Ts=1/Fs; 
+%V=10;                  %Time Transmission window (TW) 
+t=0:Ts:(81920*Ts)-Ts;    %Time axis for TW 
+tt=0:Ts:1024*Ts-Ts;    %Pulse time tone “ON” width 
+tz=0:Ts:7168*Ts-Ts;    %Pulse time zero “OFF” width 
+s1=cos(2*pi*697*tt)+cos(2*pi*1209*tt); 
+s3=cos(2*pi*697*tt)+cos(2*pi*1477*tt); 
+s8=cos(2*pi*852*tt)+cos(2*pi*1336*tt); 
+s0=cos(2*pi*941*tt)+cos(2*pi*1336*tt); 
+sz=0*tz;          %Pulse signal segment “OFF” 
+s=[s3 sz s8 sz s1 sz s1 sz s8 sz s8 sz s3 sz s0 sz s3 sz s0 sz]; 
+figure
+plot(t,s) 
+axis([0 max(t) -max(abs(s)) max(abs(s))]) 
+xlabel('Time in Seconds') 
+ylabel('Amplitude') 
+title('DTFM Message or Word of Order or Length L=10 and Duty Cycle=3/8 to 1/8') 
+sound(s,Fs) 
---
+++ INEL4301_MCW02_S096_GP01/s096g01dc34dtmf.m
@@ -0,0 +1,46 @@
+%s096g01dc34dtmf 
+%clear all 
+%close all 
+Fs=8192; 
+Ts=1/Fs; 
+%V=10;           %Time Transmission window (TW) 
+t=0:Ts:(81920*Ts)-Ts;    %Time axis for TW 
+tt=0:Ts:(6144*Ts)-Ts;    %Pulse time tone “ON” width 
+tz=0:Ts:(2048*Ts)-Ts;    %Pulse time zero “OFF” width 
+s1=cos(2*pi*697*tt)+cos(2*pi*1209*tt); 
+s3=cos(2*pi*697*tt)+cos(2*pi*1477*tt); 
+s8=cos(2*pi*852*tt)+cos(2*pi*1336*tt); 
+s0=cos(2*pi*941*tt)+cos(2*pi*1336*tt); 
+sz=0*tz;          %Pulse signal segment “OFF” 
+s=[s1 sz s3 sz s8 sz s8 sz s3 sz s3 sz s1 sz s0 sz s0 sz s1 sz]; 
+figure
+plot(t,s)
+axis([0 max(t) -max(abs(s)) max(abs(s))]) 
+xlabel('Time in Seconds') 
+ylabel('Amplitude') 
+title('DTFM Message or Word of Order or Length L=10 and Duty Cycle=1/2 to 3/4') 
+sound(s,Fs)
+
+
+%s096g01dc34dtmf 
+%clear all 
+%close all 
+Fs=8192; 
+Ts=1/Fs; 
+%V=10;                 %Time Transmission window (TW) 
+t=0:Ts:(81920*Ts)-Ts;  %Time axis for TW 
+tt=0:Ts:6144*Ts-Ts;    %Pulse time tone “ON” width 
+tz=0:Ts:2048*Ts-Ts;    %Pulse time zero “OFF” width 
+s1=cos(2*pi*697*tt)+cos(2*pi*1209*tt); 
+s3=cos(2*pi*697*tt)+cos(2*pi*1477*tt); 
+s8=cos(2*pi*852*tt)+cos(2*pi*1336*tt); 
+s0=cos(2*pi*941*tt)+cos(2*pi*1336*tt); 
+sz=0*tz;          %Pulse signal segment “OFF” 
+s=[s1 sz s3 sz s8 sz s8 sz s3 sz s3 sz s1 sz s0 sz s0 sz s1 sz]; 
+figure
+plot(t,s) 
+axis([0 max(t) -max(abs(s)) max(abs(s))]) 
+xlabel('Time in Seconds') 
+ylabel('Amplitude') 
+title('DTFM Message or Word of Order or Length L=10 and Duty Cycle=3/8 to 3/4') 
+sound(s,Fs) 
